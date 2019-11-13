4c50a7cdd1a5a1f07e75e6ff0e56240f87af4a80
Pavel <ptupitsyn@apache.org>
2019-11-13 22:37:08 +0300

    Adding benches

---
+++ EfCoreVsDapperBench.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace efcore_vs_dapper
+{
+    public class EfCoreVsDapperBench
+    {
+        [Benchmark]
+        public void SelectEntityByIdEf()
+        {
+            
+        }
+        
+        [Benchmark]
+        public void SelectEntityByIdDapper()
+        {
+            
+        }
+        
+        [Benchmark]
+        public void SelectFieldByIdEf()
+        {
+            
+        }
+        
+        [Benchmark]
+        public void SelectFieldByIdDapper()
+        {
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using BenchmarkDotNet.Running;
 
 namespace efcore_vs_dapper
 {
@@ -6,7 +6,7 @@ namespace efcore_vs_dapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<EfCoreVsDapperBench>();
         }
     }
 }
