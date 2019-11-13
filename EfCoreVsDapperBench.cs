7f55fe545afeffbd23a4ffafa328ce5663c69489
Pavel <ptupitsyn@apache.org>
2019-11-13 23:01:49 +0300

    Implement compiled query benchmark

--- EfCoreVsDapperBench.cs
+++ EfCoreVsDapperBench.cs
@@ -3,16 +3,30 @@ using System.Linq;
 using BenchmarkDotNet.Attributes;
 using Dapper;
 using efcore_vs_dapper.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace efcore_vs_dapper
 {
     public class EfCoreVsDapperBench
     {
+        private static readonly Func<BenchContext, Person> CompiledPersonQuery = 
+            EF.CompileQuery((BenchContext ctx) => ctx.Persons.Single(p => p.Id == 1));
+        
         [Benchmark]
         public void SelectEntityByIdEf()
         {
             var ctx = new BenchContext();
             var person = ctx.Persons.Single(p => p.Id == 1);
+            if (person.Id != 1)
+            {
+                throw new Exception();
+            }
+        }
+        
+        [Benchmark]
+        public void SelectEntityByIdEfCompiled()
+        {
+            var person = CompiledPersonQuery(new BenchContext());
             if (person.Id != 1)
             {
                 throw new Exception();
