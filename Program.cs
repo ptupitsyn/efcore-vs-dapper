e07cc8990e4efdd8e1bb2a0ac5c2fff866ef94fd
Pavel <ptupitsyn@apache.org>
2019-11-13 22:33:04 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace efcore_vs_dapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
