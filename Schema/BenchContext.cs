099caf546cf75885d440361e95372d65edd19418
Pavel <ptupitsyn@apache.org>
2019-11-13 22:45:26 +0300

    Add DbContext and entity

---
+++ Schema/BenchContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace efcore_vs_dapper.Schema
+{
+    public class BenchContext : DbContext
+    {
+        public BenchContext(DbContextOptions options) : base(GetOptions())
+        {
+            // No-op.
+        }
+        
+        public DbSet<Person> Persons => Set<Person>();
+
+        private static DbContextOptions<BenchContext> GetOptions()
+        {
+            return new DbContextOptionsBuilder<BenchContext>()
+                .UseSqlite(GetInMemoryOpenSqliteConnection())
+                .Options;
+        }
+        
+        private static SqliteConnection GetInMemoryOpenSqliteConnection()
+        {
+            var connection = new SqliteConnection("Data Source=:memory:;");
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ Schema/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace efcore_vs_dapper.Schema
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        
+        public string Name { get; set; }
+    }
+}
